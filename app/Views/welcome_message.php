2e29c78613060c0f0774a671206e815f1a5f5b53
JonathanRMF <moyanojonathan2001@gmail.com>
2026-06-15 18:42:28 -0300

    Creacion del footer y nav

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>My Car</title>
+    <meta name="description" content="The small framework with powerful features">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="<?= base_url('assets/imagenes/logoMini.jpg') ?>" type="image/jpg">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= base_url('assets/css/estilos.css') ?>">
+</head>
+<body>
+
+<?php include 'header.html'; ?>
+
+<!-- CONTENT -->
+<div class="container" id="content">
+        
+</div>
+
+<?php include 'footer.html'; ?>
+
+<!-- SCRIPTS -->
+
+<script {csp-script-nonce}>
+    document.getElementById("menuToggle").addEventListener('click', toggleMenu);
+    function toggleMenu() {
+        var menuItems = document.getElementsByClassName('menu-item');
+        for (var i = 0; i < menuItems.length; i++) {
+            var menuItem = menuItems[i];
+            menuItem.classList.toggle("hidden");
+        }
+    }
+</script>
+
+<!-- -->
+
+</body>
+</html>
