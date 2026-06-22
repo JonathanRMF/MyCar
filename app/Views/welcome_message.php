f60cb236c403ef4b872ad4594ba8e6519eeb67a5
JonathanRMF <moyanojonathan2001@gmail.com>
2026-06-22 14:19:37 -0300

    correccion de css

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -5,9 +5,9 @@
     <title>My Car</title>
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="<?= base_url('assets/imagenes/logoMini.jpg') ?>" type="image/jpg">
+    <link rel="icon" href="assets/imagenes/logoMini.jpg" type="image/jpg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?= base_url('assets/css/estilos.css') ?>">
+    <link rel="stylesheet" href="assets/css/estilos.css">
 </head>
 <body>
 
@@ -18,7 +18,7 @@
     <!-- tarjeta de promocion -->
     <div class="col-md-4 mb-4">
         <div class="card h-100">
-            <img id="promocion" src="<?= base_url('assets/imagenes/promocion.png') ?>" class="card-img-top" alt="Imagen intermedia">
+            <img id="promocion" src="assets/imagenes/promocion.png" class="card-img-top" alt="Imagen intermedia">
             <div class="card-body text-center">
                 <h5 class="card-title">¡Oferta Especial!</h5>
                 <p class="card-text">Producto destacado del día</p>
